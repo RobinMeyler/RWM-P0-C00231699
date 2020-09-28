5a306d5672acf258b7511c5679ab45f8be812df1
Robin Meyler <C00231699@itcarlow.ie>
2020-09-28 11:51:58 +0100

    First commit

---
+++ Assets/Tests/CubeFilterTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class CubeFilterTest
+    {
+        [Test]
+        public void CubeFilterSimple()
+        {
+            int[] input = { 1, 3, 5, 7, 9, 999 };
+            int[] output = CubeFilter.cubes(input);
+            int[] expected = { 1, 27, 125, 343, 729, 997002999 };
+
+            foreach (var o in output)
+            {
+                Debug.Log(o);
+            }
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
